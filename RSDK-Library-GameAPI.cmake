a8af675af3f1021daca7f0c28e25924bece95ac5
TheSmallAnto <antd39487@gmail.com>
2026-03-29 23:12:17 -0400

    use the one from extra slot amy

--- RSDK-Library-GameAPI.cmake
+++ RSDK-Library-GameAPI.cmake
@@ -2,7 +2,17 @@ set(WITH_RSDK OFF)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -O3")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -O3")
 
-add_executable(${GAME_NAME} ${GAME_SOURCES})
+add_executable(${GAME_NAME} 
+    ${GAME_NAME}/Game.c
+    ${GAME_NAME}/Objects/All.c
+)
+
+target_include_directories(${GAME_NAME} PRIVATE
+    ${GAME_NAME}/
+    ${GAME_NAME}/Objects/
+)
+
+target_compile_options(${GAME_NAME} PRIVATE -DRETRO_REVISION=3 -DMANIA_PREPLUS=0 -DRETRO_REV02=1 -DRETRO_REV0U=1 -DRETRO_VER_EGS=1 -DRETRO_USE_MOD_LOADER=1)
 
 set_target_properties(${GAME_NAME} PROPERTIES
     CXX_STANDARD 17
@@ -21,13 +31,11 @@ set(emsc_link_options
     -sUSE_PTHREADS=1
     -sPTHREAD_POOL_SIZE=4
     -pthread
-    -g
-    -sASYNCIFY
-    -sINVOKE_RUN=0
 )
 
 target_link_options(${GAME_NAME} PRIVATE ${emsc_link_options})
 
 set_target_properties(${GAME_NAME} PROPERTIES
+    OUTPUT_NAME "Game"
     SUFFIX ".wasm"
 )
