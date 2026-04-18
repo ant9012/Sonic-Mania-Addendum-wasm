76784320ab9ff23f0bea4868658df58ace4d3a9d
TheSmallAnto <antd39487@gmail.com>
2026-04-18 01:42:10 -0400

    Refactor CMake configuration for game build

--- RSDK-Library-GameAPI.cmake
+++ RSDK-Library-GameAPI.cmake
@@ -2,17 +2,7 @@ set(WITH_RSDK OFF)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -O3")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -O3")
 
-add_executable(${GAME_NAME} 
-    ${GAME_NAME}/Game.c
-    ${GAME_NAME}/Objects/All.c
-)
-
-target_include_directories(${GAME_NAME} PRIVATE
-    ${GAME_NAME}/
-    ${GAME_NAME}/Objects/
-)
-
-target_compile_options(${GAME_NAME} PRIVATE -DRETRO_REVISION=3 -DMANIA_PREPLUS=0 -DRETRO_REV02=1 -DRETRO_REV0U=1 -DRETRO_VER_EGS=1 -DRETRO_USE_MOD_LOADER=1)
+add_executable(${GAME_NAME} ${GAME_SOURCES})
 
 set_target_properties(${GAME_NAME} PROPERTIES
     CXX_STANDARD 17
@@ -20,24 +10,27 @@ set_target_properties(${GAME_NAME} PROPERTIES
 )
 
 set(emsc_link_options
-    -sTOTAL_MEMORY=32MB
-    -sALLOW_MEMORY_GROWTH=1
-    -sWASM=1
-    -sLINKABLE=1
-    -sEXPORT_ALL=1
     -sSIDE_MODULE=2
-    -sSINGLE_FILE=1
-    -sBINARYEN_ASYNC_COMPILATION=0
+    -sWASM=1
+
     -sUSE_PTHREADS=1
+    -sIMPORTED_MEMORY=1
+    -sSHARED_MEMORY=1
     -sPTHREAD_POOL_SIZE=4
     -pthread
+
+    -sEXPORT_ALL=1
+    -Wl,--export-all
+    -Wl,--no-gc-sections
+
+    -sERROR_ON_UNDEFINED_SYMBOLS=0
+    -sALLOW_MEMORY_GROWTH=1
+
     -g
-    -sEMULATE_FUNCTION_POINTER_CASTS=1
 )
 
 target_link_options(${GAME_NAME} PRIVATE ${emsc_link_options})
 
 set_target_properties(${GAME_NAME} PROPERTIES
-    OUTPUT_NAME "Game"
     SUFFIX ".wasm"
 )
